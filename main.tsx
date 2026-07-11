9d194ab8c3d496aba2d947717b8e21d2c0e273c0
AdalRomero <adalbertoromero1245@gmail.com>
2026-07-11 01:56:47 -0700

    rutas basicas

--- main.tsx
+++ main.tsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './app/css/index.css'
 import Home from './app/layout/home'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Home />
+      <Routes>
+        <Route path="/*" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 )
